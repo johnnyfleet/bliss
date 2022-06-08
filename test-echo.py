2cfa6de5ba2da228974ad90d3ae38d67ca414cd8
John Stephenson <johnnyfleet@gmail.com>
2022-06-08 02:13:51 +0000

    Added link to website

--- test-echo.py
+++ test-echo.py
@@ -1,5 +1,8 @@
+# See: https://stackoverflow.com/questions/61656704/github-action-use-existing-python-script-and-get-string-output
+
 import sys
 
+#print("argument {}".format(sys.argv[1]))
 print("changelog 1")
 print("changelog 2")
 print("changelog 3")
